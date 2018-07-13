e57f684338d8606539d48588c65e86f84c1ceac2
mchimenti <michael.chimenti@gmail.com>
2018-07-13 13:23:49 -0500

    Initial commit

---
+++ usachev_sleuth_rnaseq.r
@@ -0,0 +1,15 @@
+## Analysis of Usachev/zhihong RNA-Seq mouse MCU knockout in hippocampus and cortex
+## Date: 7.13.2018
+## Author: Michael Chimenti
+## Organism: mm10 / mouse 
+## Aligners: hisat2 / salmon
+## Design: balanced case/control in two tissues: hip and cortex
+## Reps: 3
+## total samples: 12
+
+##########
+## Imports
+##########
+
+#source("http://bioconductor.org/biocLite.R")
+
